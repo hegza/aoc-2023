0f6900b3a981c53fc4b911406d950d37d412a59b
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2023-12-02 07:46:04 +0200

    Day 2 cleanup

--- examples/day2.rs
+++ examples/day2.rs
@@ -7,77 +7,21 @@ const GREEN: i64 = 13;
 const BLUE: i64 = 14;
 
 fn main() -> anyhow::Result<()> {
-    let mut lines = INPUT.lines();
-
-    let mut sum = 0;
-    while let Some(line) = lines.next() {
-        //
-        let (game_s, rest) = line.split_once(':').unwrap();
-        let game_n = &Regex::new(r"Game (\d+)")
-            .unwrap()
-            .captures_iter(game_s)
-            .next()
-            .unwrap()[1]
-            .parse::<i32>()
-            .unwrap();
-
-        let mut rmax = 0;
-        let mut bmax = 0;
-        let mut gmax = 0;
-
-        let mut it = rest.split(';');
-        while let Some(set) = it.next() {
-            let r = match Regex::new(r"(\d+) red").unwrap().captures_iter(set).next() {
-                Some(x) => x[1].parse::<i64>().unwrap(),
-                None => 0,
-            };
-            if r > rmax {
-                rmax = r;
-            }
-            let b = match Regex::new(r"(\d+) blue").unwrap().captures_iter(set).next() {
-                Some(x) => x[1].parse::<i64>().unwrap(),
-                None => 0,
-            };
-            if b > bmax {
-                bmax = b;
-            }
-            let g = match Regex::new(r"(\d+) green")
-                .unwrap()
-                .captures_iter(set)
-                .next()
-            {
-                Some(x) => x[1].parse::<i64>().unwrap(),
-                None => 0,
-            };
-            if g > gmax {
-                gmax = g;
-            }
-        }
-        //if rmax <= RED && gmax <= GREEN && bmax <= BLUE {
-        let p = rmax * gmax * bmax;
-        println!("{game_n}: {p}");
-        sum += p;
-        //}
-    }
-    println!("{sum}");
+    println!("Part 1: {}", part1(INPUT.lines())?);
+    println!("Part 2: {}", part2(INPUT.lines())?);
 
     Ok(())
 }
 
-fn part1() {
-    let mut lines = INPUT.lines();
-
+fn part1(mut lines: std::str::Lines<'_>) -> anyhow::Result<i64> {
     let mut sum = 0;
     while let Some(line) = lines.next() {
-        //
         let (game_s, rest) = line.split_once(':').unwrap();
-        let game_n = &Regex::new(r"Game (\d+)")
-            .unwrap()
+        let game_n = &Regex::new(r"Game (\d+)")?
             .captures_iter(game_s)
             .next()
             .unwrap()[1]
-            .parse::<i32>()
-            .unwrap();
+            .parse::<i64>()?;
 
         let mut rmax = 0;
         let mut bmax = 0;
@@ -85,26 +29,22 @@ fn part1() {
 
         let mut it = rest.split(';');
         while let Some(set) = it.next() {
-            let r = match Regex::new(r"(\d+) red").unwrap().captures_iter(set).next() {
-                Some(x) => x[1].parse::<i64>().unwrap(),
+            let r = match Regex::new(r"(\d+) red")?.captures_iter(set).next() {
+                Some(x) => x[1].parse::<i64>()?,
                 None => 0,
             };
             if r > rmax {
                 rmax = r;
             }
-            let b = match Regex::new(r"(\d+) blue").unwrap().captures_iter(set).next() {
-                Some(x) => x[1].parse::<i64>().unwrap(),
+            let b = match Regex::new(r"(\d+) blue")?.captures_iter(set).next() {
+                Some(x) => x[1].parse::<i64>()?,
                 None => 0,
             };
             if b > bmax {
                 bmax = b;
             }
-            let g = match Regex::new(r"(\d+) green")
-                .unwrap()
-                .captures_iter(set)
-                .next()
-            {
-                Some(x) => x[1].parse::<i64>().unwrap(),
+            let g = match Regex::new(r"(\d+) green")?.captures_iter(set).next() {
+                Some(x) => x[1].parse::<i64>()?,
                 None => 0,
             };
             if g > gmax {
@@ -115,5 +55,46 @@ fn part1() {
             sum += game_n;
         }
     }
-    println!("{sum}");
+
+    Ok(sum)
+}
+
+fn part2(mut lines: std::str::Lines<'_>) -> anyhow::Result<i64> {
+    let mut sum = 0;
+    while let Some(line) = lines.next() {
+        let (_, rest) = line.split_once(':').unwrap();
+
+        let mut rmax = 0;
+        let mut bmax = 0;
+        let mut gmax = 0;
+
+        let mut it = rest.split(';');
+        while let Some(set) = it.next() {
+            let r = match Regex::new(r"(\d+) red")?.captures_iter(set).next() {
+                Some(x) => x[1].parse::<i64>()?,
+                None => 0,
+            };
+            if r > rmax {
+                rmax = r;
+            }
+            let b = match Regex::new(r"(\d+) blue")?.captures_iter(set).next() {
+                Some(x) => x[1].parse::<i64>()?,
+                None => 0,
+            };
+            if b > bmax {
+                bmax = b;
+            }
+            let g = match Regex::new(r"(\d+) green")?.captures_iter(set).next() {
+                Some(x) => x[1].parse::<i64>()?,
+                None => 0,
+            };
+            if g > gmax {
+                gmax = g;
+            }
+        }
+        let p = rmax * gmax * bmax;
+        sum += p;
+    }
+
+    Ok(sum)
 }
