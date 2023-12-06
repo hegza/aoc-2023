e75545182a169ad4ce5fc4b2b829208a09b8fdac
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2023-12-06 07:57:24 +0200

    Day 6 cleanup

--- examples/day6.rs
+++ examples/day6.rs
@@ -1,6 +1,3 @@
-use itertools::Itertools;
-use std::iter;
-
 const INPUT: &str = include_str!("inputs/day6.txt");
 
 fn main() -> anyhow::Result<()> {
@@ -12,69 +9,33 @@ fn main() -> anyhow::Result<()> {
         .split_once(':')
         .unwrap()
         .1
-        .split_whitespace()
-        .collect_vec();
-    let time = times.concat().parse::<i64>()?;
+        .split_whitespace();
     let distances = lines
         .next()
         .unwrap()
         .split_once(':')
         .unwrap()
         .1
-        .split_whitespace()
-        .collect_vec();
-    let distance = distances.concat().parse::<i64>()?;
+        .split_whitespace();
 
-    println!("Times: {:?}\n\nDistances: {:?}", time, distance);
-
-    let s: usize = iter::once(time)
-        .zip(iter::once(distance))
-        .map(|(t, m)| {
-            let held_count = (1..t - 1)
-                .filter(|&h| {
-                    let dist = h * (t - h + 1) - h;
-                    dist > m
-                })
-                .count();
-            held_count
-        })
-        .product();
-
-    println!("{s}");
+    println!(
+        "Part 1: {}",
+        part1::solve(times.clone(), distances.clone())?
+    );
+    println!("Part 2: {}", part2::solve(times, distances)?);
 
     Ok(())
 }
 
-/*
 mod part1 {
-    use crate::INPUT;
+    pub(crate) fn solve<'a>(
+        times: impl Iterator<Item = impl AsRef<str>>,
+        distances: impl Iterator<Item = impl AsRef<str>>,
+    ) -> anyhow::Result<i64> {
+        let times = times.map(|s| s.as_ref().parse::<i64>().unwrap());
+        let distances = distances.map(|s| s.as_ref().parse::<i64>().unwrap());
 
-    fn solve() -> anyhow::Result<()> {
-        let mut lines = INPUT.lines();
-
-        let times = lines
-            .next()
-            .unwrap()
-            .split_once(':')
-            .unwrap()
-            .1
-            .split_whitespace()
-            .map(|s| s.parse::<i64>().unwrap())
-            .collect_vec();
-        let distances = lines
-            .next()
-            .unwrap()
-            .split_once(':')
-            .unwrap()
-            .1
-            .split_whitespace()
-            .map(|s| s.parse::<i64>().unwrap())
-            .collect_vec();
-
-        println!("Times: {:?}\n\nDistances: {:?}", times, distances);
-
-        let s: usize = times
-            .into_iter()
+        let s: i64 = times
             .zip(distances)
             .map(|(t, m)| {
                 let held_count = (1..t - 1)
@@ -83,13 +44,25 @@ mod part1 {
                         dist > m
                     })
                     .count();
-                held_count
+                held_count as i64
             })
             .product();
 
-        println!("{s}");
-
-        Ok(())
+        Ok(s)
     }
 }
-*/
+
+mod part2 {
+    use crate::part1;
+    use std::iter;
+
+    pub(crate) fn solve<'a>(
+        times: impl Iterator<Item = &'a str>,
+        distances: impl Iterator<Item = &'a str>,
+    ) -> anyhow::Result<i64> {
+        part1::solve(
+            iter::once(times.collect::<String>()),
+            iter::once(distances.collect::<String>()),
+        )
+    }
+}
