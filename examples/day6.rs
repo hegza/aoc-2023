b58041f8a14ff4ff5fb7b6844d2e4e0ae2328c8b
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2023-12-06 07:43:01 +0200

    Day 6 part 2 raw

--- examples/day6.rs
+++ examples/day6.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::collections::*;
+use std::iter;
 
 const INPUT: &str = include_str!("inputs/day6.txt");
 
@@ -13,8 +13,8 @@ fn main() -> anyhow::Result<()> {
         .unwrap()
         .1
         .split_whitespace()
-        .map(|s| s.parse::<i64>().unwrap())
         .collect_vec();
+    let time = times.concat().parse::<i64>()?;
     let distances = lines
         .next()
         .unwrap()
@@ -22,14 +22,13 @@ fn main() -> anyhow::Result<()> {
         .unwrap()
         .1
         .split_whitespace()
-        .map(|s| s.parse::<i64>().unwrap())
         .collect_vec();
+    let distance = distances.concat().parse::<i64>()?;
 
-    println!("Times: {:?}\n\nDistances: {:?}", times, distances);
+    println!("Times: {:?}\n\nDistances: {:?}", time, distance);
 
-    let s: usize = times
-        .into_iter()
-        .zip(distances)
+    let s: usize = iter::once(time)
+        .zip(iter::once(distance))
         .map(|(t, m)| {
             let held_count = (1..t - 1)
                 .filter(|&h| {
@@ -45,3 +44,52 @@ fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+/*
+mod part1 {
+    use crate::INPUT;
+
+    fn solve() -> anyhow::Result<()> {
+        let mut lines = INPUT.lines();
+
+        let times = lines
+            .next()
+            .unwrap()
+            .split_once(':')
+            .unwrap()
+            .1
+            .split_whitespace()
+            .map(|s| s.parse::<i64>().unwrap())
+            .collect_vec();
+        let distances = lines
+            .next()
+            .unwrap()
+            .split_once(':')
+            .unwrap()
+            .1
+            .split_whitespace()
+            .map(|s| s.parse::<i64>().unwrap())
+            .collect_vec();
+
+        println!("Times: {:?}\n\nDistances: {:?}", times, distances);
+
+        let s: usize = times
+            .into_iter()
+            .zip(distances)
+            .map(|(t, m)| {
+                let held_count = (1..t - 1)
+                    .filter(|&h| {
+                        let dist = h * (t - h + 1) - h;
+                        dist > m
+                    })
+                    .count();
+                held_count
+            })
+            .product();
+
+        println!("{s}");
+
+        Ok(())
+    }
+}
+*/
