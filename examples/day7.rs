64a175ce9de2f633d7ddad749d981a7d2461af7e
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2023-12-07 09:20:36 +0200

    Day 7 cleanup

--- examples/day7.rs
+++ examples/day7.rs
@@ -1,42 +1,40 @@
 use itertools::Itertools;
-use std::{cmp::Ordering, collections::*, str::FromStr};
+use std::cmp::{self, Ordering};
 
 const INPUT: &str = include_str!("inputs/day7.txt");
 
-#[derive(PartialEq, Eq, Hash, Debug)]
-struct Card(char);
+fn main() -> anyhow::Result<()> {
+    let p1 = part1::solve()?;
+    println!("Part 1: {}", p1);
+    assert_eq!(p1, 246912307);
 
-impl Card {
-    fn value(&self) -> usize {
-        match self.0 {
-            c if c.is_numeric() => c.to_digit(10).unwrap() as usize,
-            'T' => 10,
-            'Q' => 12,
-            'K' => 13,
-            'A' => 14,
-            'J' => 1,
-            _ => panic!("not a card"),
+    let p2 = part2::solve()?;
+    println!("Part 2: {}", p2);
+    assert_eq!(p2, 246894760);
+
+    Ok(())
+}
+
+fn card_value(card: &char, joker_rule: bool) -> usize {
+    match card {
+        c if c.is_numeric() => c.to_digit(10).unwrap() as usize,
+        'T' => 10,
+        'J' => {
+            if joker_rule {
+                1
+            } else {
+                11
+            }
         }
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => panic!("not a card"),
     }
 }
 
-impl PartialOrd for Card {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        let v = self.value();
-        let v2 = other.value();
-        v.partial_cmp(&v2)
-    }
-}
-
-#[derive(PartialEq, Debug)]
-struct Hand(Vec<Card>);
-
-impl FromStr for Hand {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self(s.chars().map(|c| Card(c)).collect_vec()))
-    }
+fn card_cmp(left: &char, right: &char, joker_rule: bool) -> cmp::Ordering {
+    card_value(left, joker_rule).cmp(&card_value(right, joker_rule))
 }
 
 #[derive(PartialEq, PartialOrd, Debug)]
@@ -51,179 +49,156 @@ enum Kind {
     None = 0,
 }
 
-impl Hand {
-    fn can_be_high(&self) -> bool {
-        let cs = &self.0;
+fn can_be_house(hand: &[char]) -> bool {
+    let nonj_counts = hand
+        .iter()
+        .unique()
+        .filter(|&&x| x != 'J')
+        .map(|c1| hand.iter().filter(|&c2| c2 == c1).count())
+        .collect_vec();
+    let pairs = nonj_counts.iter().filter(|&&count| count == 2).count();
 
-        cs.iter()
-            .unique()
-            .filter(|&x| x.0 != 'J')
-            .all(|c1| cs.iter().filter(|&c2| (c1.0 == c2.0)).count() == 1)
-    }
-
-    fn can_be_house(&self) -> bool {
-        let cs = &self.0;
-
-        let nonj_counts = cs
+    let jcount = hand.iter().filter(|&&c| c == 'J').count();
+    match jcount {
+        5 | 4 | 3 => true,
+        2 => !hand
             .iter()
             .unique()
-            .filter(|x| x.0 != 'J')
-            .map(|c1| cs.iter().filter(|&c2| c2 == c1).count())
-            .collect_vec();
-        let pairs = nonj_counts.iter().filter(|&&count| count == 2).count();
-
-        let jcount = cs.iter().filter(|c| c.0 == 'J').count();
-        match jcount {
-            5 | 4 | 3 => true,
-            2 => !self.can_be_high(),
-            1 => pairs >= 2,
-            0 => nonj_counts.contains(&3) && nonj_counts.contains(&2),
-            _ => panic!(),
-        }
-    }
-
-    fn kind_with_joker_rule(&self) -> Kind {
-        let cs = &self.0;
-
-        let nonj_counts = cs
-            .iter()
-            .unique()
-            .map(|c1| cs.iter().filter(|&c2| (c2 == c1 || c2.0 == 'J')).count())
-            .collect_vec();
-        let of_kind = *nonj_counts.iter().max().unwrap();
-        let pairs = nonj_counts.iter().filter(|&&count| count == 2).count();
-
-        let jcount = cs.iter().filter(|c| c.0 == 'J').count();
-
-        if of_kind == 5 {
-            Kind::Five
-        } else if of_kind == 4 {
-            Kind::Four
-        } else if self.can_be_house() {
-            Kind::House
-        } else if of_kind == 3 {
-            Kind::Three
-        } else if pairs == 2 || jcount >= 2 {
-            Kind::TwoPair
-        } else if jcount != 0 {
-            Kind::OnePair
-        } else if self.can_be_high() {
-            Kind::High
-        } else {
-            Kind::None
-        }
-    }
-
-    fn kind_without_joker_rule(&self) -> Kind {
-        let cs = &self.0;
-
-        let counts = cs
-            .iter()
-            .unique()
-            .map(|c1| cs.iter().filter(|&c2| c2 == c1).count())
-            .collect_vec();
-        let of_kind = *counts.iter().max().unwrap();
-        let pairs = counts.iter().filter(|&&count| count == 2).count();
-
-        if of_kind == 5 {
-            Kind::Five
-        } else if of_kind == 4 {
-            Kind::Four
-        } else if counts.contains(&2) && counts.contains(&3) {
-            Kind::House
-        } else if of_kind == 3 {
-            Kind::Three
-        } else if pairs == 2 {
-            Kind::TwoPair
-        } else if pairs == 1 {
-            Kind::OnePair
-        } else if counts
-            .iter()
-            .all(|c1| counts.iter().filter(|&c2| c1 == c2).count() == 1)
-        {
-            Kind::High
-        } else {
-            Kind::None
-        }
-    }
-
-    fn value(&self) -> usize {
-        (self.kind_with_joker_rule() as usize).max(self.kind_without_joker_rule() as usize)
+            .filter(|&&x| x != 'J')
+            .all(|c1| hand.iter().filter(|&c2| (c1 == c2)).count() == 1),
+        1 => pairs >= 2,
+        0 => nonj_counts.contains(&3) && nonj_counts.contains(&2),
+        _ => panic!(),
     }
 }
 
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        match self.value().partial_cmp(&other.value()).unwrap() {
-            ord @ (Ordering::Less | Ordering::Greater) => Some(ord),
-            Ordering::Equal => {
-                for (c1, c2) in self.0.iter().zip(other.0.iter()) {
-                    match c1.partial_cmp(c2).unwrap() {
-                        Ordering::Equal => continue,
-                        ord => {
-                            return Some(ord);
-                        }
+fn kind_with_joker_rule(hand: &[char]) -> Kind {
+    let counts = hand
+        .iter()
+        .unique()
+        .map(|&c1| hand.iter().filter(|&&c2| (c2 == c1 || c2 == 'J')).count())
+        .collect_vec();
+    let of_kind = *counts.iter().max().unwrap();
+    let pairs = counts.iter().filter(|&&count| count == 2).count();
+
+    let jcount = hand.iter().filter(|&&c| c == 'J').count();
+
+    if of_kind == 5 {
+        Kind::Five
+    } else if of_kind == 4 {
+        Kind::Four
+    } else if can_be_house(hand) {
+        Kind::House
+    } else if of_kind == 3 {
+        Kind::Three
+    } else if pairs == 2 || jcount >= 2 {
+        Kind::TwoPair
+    } else if jcount != 0 {
+        Kind::OnePair
+    } else if hand
+        .iter()
+        .unique()
+        .filter(|&&x| x != 'J')
+        .all(|c1| hand.iter().filter(|&c2| (c1 == c2)).count() == 1)
+    {
+        Kind::High
+    } else {
+        Kind::None
+    }
+}
+
+fn kind_without_joker_rule(hand: &[char]) -> Kind {
+    let counts = hand
+        .iter()
+        .unique()
+        .map(|c1| hand.iter().filter(|&c2| c2 == c1).count())
+        .collect_vec();
+    let of_kind = *counts.iter().max().unwrap();
+    let pairs = counts.iter().filter(|&&count| count == 2).count();
+
+    if of_kind == 5 {
+        Kind::Five
+    } else if of_kind == 4 {
+        Kind::Four
+    } else if counts.contains(&2) && counts.contains(&3) {
+        Kind::House
+    } else if of_kind == 3 {
+        Kind::Three
+    } else if pairs == 2 {
+        Kind::TwoPair
+    } else if pairs == 1 {
+        Kind::OnePair
+    } else if counts
+        .iter()
+        .all(|c1| counts.iter().filter(|&c2| c1 == c2).count() == 1)
+    {
+        Kind::High
+    } else {
+        Kind::None
+    }
+}
+
+fn value(hand: &[char], joker_rule: bool) -> usize {
+    if joker_rule {
+        (kind_with_joker_rule(hand) as usize).max(kind_without_joker_rule(hand) as usize)
+    } else {
+        kind_without_joker_rule(hand) as usize
+    }
+}
+
+fn hand_cmp(left: &[char], right: &[char], joker_rule: bool) -> cmp::Ordering {
+    match value(left, joker_rule).cmp(&value(right, joker_rule)) {
+        ord @ (Ordering::Less | Ordering::Greater) => ord,
+        Ordering::Equal => {
+            for (c1, c2) in left.iter().zip(right.iter()) {
+                match card_cmp(c1, c2, joker_rule) {
+                    Ordering::Equal => continue,
+                    ord => {
+                        return ord;
                     }
                 }
-                return None;
             }
+            Ordering::Equal
         }
     }
-}
-
-fn main() -> anyhow::Result<()> {
-    let lines = INPUT.lines();
-
-    let mut hands = lines
-        .into_iter()
-        .map(|line| {
-            let (hand, bid) = line.split_once(' ').unwrap();
-
-            let hand = Hand::from_str(hand).unwrap();
-            let bid = bid.parse::<usize>().unwrap();
-
-            (hand, bid)
-        })
-        .collect_vec();
-
-    hands.sort_by(|(hand1, _), (hand2, _)| {
-        hand1.partial_cmp(&hand2).expect("cannot order elements")
-    });
-
-    for (rank, hand) in hands
-        .iter()
-        .map(|(h, bid)| {
-            (
-                h.0.iter().map(|c| c.0).collect_vec(),
-                h.kind_with_joker_rule(),
-                bid,
-            )
-        })
-        //.filter(|(h, _, _)| h.iter().filter(|&&c| c == 'J').count() == 2)
-        .enumerate()
-    {
-        println!("{}: {:?}", rank + 1, &hand);
-    }
-
-    let winnings: usize = hands
-        .into_iter()
-        .enumerate()
-        .map(|(idx, (_hand, bid))| {
-            let rank = idx + 1;
-            bid * rank
-        })
-        .sum::<usize>();
-
-    println!("{winnings}");
-    Ok(())
 }
 
 mod part1 {
-    use crate::{Hand, INPUT};
+    use crate::{hand_cmp, INPUT};
     use itertools::Itertools;
-    use std::str::FromStr;
 
-    fn solve() -> anyhow::Result<()> {
+    pub(crate) fn solve() -> anyhow::Result<i64> {
+        let lines = INPUT.lines();
+        let mut hands = lines
+            .into_iter()
+            .map(|line| {
+                let (hand, bid) = line.split_once(' ').unwrap();
+
+                let hand = hand.chars().collect_vec();
+                let bid = bid.parse::<usize>().unwrap();
+
+                (hand, bid)
+            })
+            .collect_vec();
+
+        hands.sort_by(|(hand1, _), (hand2, _)| hand_cmp(hand1, hand2, false));
+
+        let hands = hands
+            .into_iter()
+            .enumerate()
+            .map(|(idx, rest)| (idx + 1, rest));
+
+        let winnings: usize = hands.map(|(rank, (_, bid))| bid * rank).sum::<usize>();
+
+        Ok(winnings as i64)
+    }
+}
+
+mod part2 {
+    use crate::{hand_cmp, INPUT};
+    use itertools::Itertools;
+
+    pub(crate) fn solve() -> anyhow::Result<i64> {
         let lines = INPUT.lines();
 
         let mut hands = lines
@@ -231,37 +206,22 @@ mod part1 {
             .map(|line| {
                 let (hand, bid) = line.split_once(' ').unwrap();
 
-                let hand = Hand::from_str(hand).unwrap();
+                let hand = hand.chars().collect_vec();
                 let bid = bid.parse::<usize>().unwrap();
 
                 (hand, bid)
             })
             .collect_vec();
 
-        hands.sort_by(|(hand1, _), (hand2, _)| {
-            hand1.partial_cmp(&hand2).expect("cannot order elements")
-        });
+        hands.sort_by(|(hand1, _), (hand2, _)| hand_cmp(hand1, hand2, true));
 
-        /*
-        for (rank, hand) in hands
-            .iter()
-            .map(|(h, bid)| (h.0.iter().map(|c| c.0).collect_vec(), h.kind(), bid))
-            .enumerate()
-        {
-            println!("{}: {:?}", rank + 1, &hand);
-        }*/
-
-        /*
-        let winnings: usize = hands
+        let hands = hands
             .into_iter()
             .enumerate()
-            .map(|(idx, (_hand, bid))| {
-                let rank = idx + 1;
-                bid * rank
-            })
-            .sum::<usize>();
+            .map(|(idx, rest)| (idx + 1, rest));
 
-        println!("{winnings}");*/
-        Ok(())
+        let winnings: usize = hands.map(|(rank, (_, bid))| bid * rank).sum::<usize>();
+
+        Ok(winnings as i64)
     }
 }
