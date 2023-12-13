dd764180f9e2de3c5b1ac1798e220c72dccbf3da
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2023-12-13 09:53:33 +0200

    Day 13 part w raw

--- examples/day13.rs
+++ examples/day13.rs
@@ -2,19 +2,17 @@ use itertools::Itertools;
 
 const INPUT: &str = include_str!("inputs/day13.txt");
 
-fn transpose(v: &Vec<String>) -> Vec<String> {
-    assert!(!v.is_empty());
-
-    let mut ret = vec![String::new(); v[0].len()];
-    for line in v {
-        line.chars().enumerate().for_each(|(idx, c)| {
-            ret[idx].push(c);
-        });
-    }
+fn transpose<T: Clone>(v: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let mut ret = vec![Vec::new(); v[0].len()];
+    v.iter().for_each(|line| {
+        line.iter().enumerate().for_each(|(idx, c)| {
+            ret[idx].push(c.clone());
+        })
+    });
     ret
 }
 
-fn candidates(lines: &[String]) -> impl Iterator<Item = usize> + Clone + '_ {
+fn seams(lines: &[Vec<bool>]) -> impl Iterator<Item = usize> + Clone + '_ {
     lines
         .iter()
         .tuple_windows()
@@ -22,14 +20,18 @@ fn candidates(lines: &[String]) -> impl Iterator<Item = usize> + Clone + '_ {
         .map(|x| x + 1)
 }
 
-fn is_mirror(line: usize, pat: &[String]) -> bool {
+fn is_mirror(line: usize, pat: &[Vec<bool>]) -> bool {
     let rowcount = line.min(pat.len() - line);
 
     (0..rowcount).all(|row| pat[line - row - 1] == pat[line + row])
 }
 
-fn main() -> anyhow::Result<()> {
-    let mut lines = INPUT.lines();
+fn find_mirror(pat: &[Vec<bool>]) -> Option<usize> {
+    seams(&pat).find(|line| is_mirror(*line, pat))
+}
+
+fn parse(input: &str) -> Vec<Vec<Vec<bool>>> {
+    let mut lines = input.lines();
 
     let mut patterns = vec![];
     let mut cur_pat = vec![];
@@ -38,40 +40,87 @@ fn main() -> anyhow::Result<()> {
             patterns.push(cur_pat);
             cur_pat = vec![];
         } else {
-            cur_pat.push(line.to_owned())
+            cur_pat.push(
+                line.chars()
+                    .map(|c| match c {
+                        '#' => true,
+                        '.' => false,
+                        _ => panic!(),
+                    })
+                    .collect_vec(),
+            )
         }
     }
     patterns.push(cur_pat);
+    patterns
+}
 
-    let tpatterns = patterns.iter().map(|pattern| transpose(pattern));
+fn main() -> anyhow::Result<()> {
+    let patterns = parse(&INPUT);
 
-    let sum: usize = patterns
-        .iter()
-        .zip(tpatterns)
-        .enumerate()
-        .map(|(pat_idx, (pat, tpat))| {
-            let mut _candidates = candidates(&pat);
-            /*println!(
-                "hlines in pattern {pat_idx} at {:?}",
-                &_candidates.clone().collect_vec()
-            );*/
-            if let Some(line) = _candidates.find(|line| is_mirror(*line, &pat)) {
-                println!("hmirror in pattern {pat_idx} at {line}");
-                line * 100
-            } else {
-                let mut _candidates = candidates(&tpat);
-                /*println!(
-                    "vlines in pattern {pat_idx} at {:?}",
-                    &_candidates.clone().collect_vec()
-                );*/
-                let line = _candidates.find(|line| is_mirror(*line, &tpat)).unwrap();
-                println!("vmirror in pattern {pat_idx} at {line}");
-                line
-            }
+    let combinations = patterns.iter().map(|pat| {
+        let w = pat[0].len();
+        let h = pat.len();
+        (0..h).cartesian_product(0..w).map(move |(row, col)| {
+            let mut npat = pat.clone();
+            // Flip one char
+            npat[row][col] = !npat[row][col];
+            npat
         })
-        .sum();
+    });
+
+    let line_scores = patterns.iter().zip(combinations).map(|(orig, many_pats)| {
+        let torig = transpose(&orig);
+        many_pats
+            .map(|pat| (pat.clone(), transpose(&pat)))
+            .find_map(|(pat, tpat)| {
+                let orig_hline = find_mirror(&orig);
+                let potential_hline = seams(&pat).find(|line| {
+                    is_mirror(*line, &pat) && !orig_hline.is_some_and(|orig| orig == *line)
+                });
+                if potential_hline.is_some() && orig_hline != potential_hline {
+                    Some(potential_hline.unwrap() * 100)
+                } else {
+                    let orig_vline = find_mirror(&torig);
+                    let potential_vline = seams(&tpat).find(|line| {
+                        is_mirror(*line, &tpat) && !orig_vline.is_some_and(|orig| orig == *line)
+                    });
+                    if potential_vline.is_some() && orig_vline != potential_vline {
+                        potential_vline
+                    } else {
+                        None
+                    }
+                }
+            })
+            .unwrap()
+    });
+
+    let sum: usize = line_scores.sum();
 
     println!("{sum}");
+    assert_eq!(sum, 31974);
 
     Ok(())
+}
+
+mod part1 {
+    use crate::{find_mirror, parse, transpose, INPUT};
+
+    pub(crate) fn solve() -> i64 {
+        let patterns = parse(&INPUT);
+        let tpatterns = patterns.iter().map(|pattern| transpose(pattern));
+
+        let sum: usize = patterns
+            .iter()
+            .zip(tpatterns)
+            .map(|(pat, tpat)| {
+                if let Some(line) = find_mirror(pat) {
+                    line * 100
+                } else {
+                    find_mirror(&tpat).unwrap()
+                }
+            })
+            .sum();
+        sum as i64
+    }
 }
